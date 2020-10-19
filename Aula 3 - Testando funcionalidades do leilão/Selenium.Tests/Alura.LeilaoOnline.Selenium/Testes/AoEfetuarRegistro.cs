42459ddf64e81e651df2ba09bad7a55cda113b3b
Lênin Müller Sampaio <lenin_muller@uol.com.br>
2020-10-19 20:44:06 -0300

    Aula 3 - Testando funcionalidades do leilão

---
+++ Aula 3 - Testando funcionalidades do leilão/Selenium.Tests/Alura.LeilaoOnline.Selenium/Testes/AoEfetuarRegistro.cs
@@ -0,0 +1,95 @@
+﻿using Alura.LeilaoOnline.Selenium.Fixtures;
+using OpenQA.Selenium;
+using Xunit;
+
+namespace Alura.LeilaoOnline.Selenium.Testes
+{
+    [Collection("Chrome Driver")]
+    public class AoEfetuarRegistro
+    {
+        private IWebDriver driver;
+
+        public AoEfetuarRegistro(TestFixture fixture)
+        {
+            driver = fixture.Driver;
+        }
+
+        [Fact]
+        public void DadoInfoValidasDeveIrParaPaginaDeAgradecimento()
+        {
+            //arrange - dado chrome aberto, página inicial do sist. de leilões, 
+            //dados de registro válidos informados
+            driver.Navigate().GoToUrl("http://localhost:5000");
+
+            //nome
+            var inputNome = driver.FindElement(By.Id("Nome"));
+
+            //email
+            var inputEmail = driver.FindElement(By.Id("Email"));
+
+            //password
+            var inputSenha = driver.FindElement(By.Id("Password"));
+
+            //confirmpassword
+            var inputConfirmSenha = driver.FindElement(By.Id("ConfirmPassword"));
+
+            //botão de registro
+            var botaoRegistro = driver.FindElement(By.Id("btnRegistro"));
+
+            inputNome.SendKeys("Daniel Portugal");
+            inputEmail.SendKeys("daniel.portugal@caelum.com.br");
+            inputSenha.SendKeys("123");
+            inputConfirmSenha.SendKeys("123");
+
+            //act - efetuo o registro
+            botaoRegistro.Click();
+
+            //assert - devo ser direcionado para uma página de agradecimento
+            Assert.Contains("Obrigado", driver.PageSource);
+
+        }
+
+        [Theory]
+        [InlineData("", "daniel.portugal@caelum.com.br", "123", "123")]
+        [InlineData("Daniel Portugal", "daniel", "123", "123")]
+        [InlineData("Daniel Portugal", "daniel.portugal@caelum.com.br", "123", "456")]
+        [InlineData("Daniel Portugal", "daniel.portugal@caelum.com.br", "123", "")]
+        public void DadoInfoinvalidasDeveContinuarNaHome(
+            string nome,
+            string email,
+            string senha,
+            string confirmSenha)
+        {
+            //arrange - dado chrome aberto, página inicial do sist. de leilões, 
+            //dados de registro válidos informados
+            driver.Navigate().GoToUrl("http://localhost:5000");
+
+            //nome
+            var inputNome = driver.FindElement(By.Id("Nome"));
+
+            //email
+            var inputEmail = driver.FindElement(By.Id("Email"));
+
+            //password
+            var inputSenha = driver.FindElement(By.Id("Password"));
+
+            //confirmpassword
+            var inputConfirmSenha = driver.FindElement(By.Id("ConfirmPassword"));
+
+            //botão de registro
+            var botaoRegistro = driver.FindElement(By.Id("btnRegistro"));
+
+            inputNome.SendKeys(nome);
+            inputEmail.SendKeys(email);
+            inputSenha.SendKeys(senha);
+            inputConfirmSenha.SendKeys(confirmSenha);
+
+            //act - efetuo o registro
+            botaoRegistro.Click();
+
+            //assert - devo ser direcionado para uma página de agradecimento
+            Assert.Contains("section-registro", driver.PageSource);
+
+        }
+    }
+}
